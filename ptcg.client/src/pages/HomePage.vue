f9a1ce29bb43ccf1ac97feb21e57de9c2cf6e23e
tri le <letrin93@gmail.com>
2023-01-06 20:44:06 -0700

    pokemon cards

--- ptcg.client/src/pages/HomePage.vue
+++ ptcg.client/src/pages/HomePage.vue
@@ -1,22 +1,14 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img
-        src="https://bcw.blob.core.windows.net/public/img/8600856373152463"
-        alt="CodeWorks Logo"
-        class="rounded-circle"
-      >
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
-  </div>
+  <!-- <div v-for="c in cards" :key="c.id">
+    <img :src="c.img" alt="">
+  </div> -->
 </template>
 
 <script>
 import { onMounted } from "vue"
 import Pop from "../utils/Pop.js"
 import { cardsService } from '../services/CardsService.js';
+import { computed } from '@vue/reactivity';
 
 export default {
   setup() {
@@ -32,29 +24,13 @@ export default {
       getCards();
     });
 
-    return {}
+    return {
+      cards: computed(() => AppState.cards)
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
 
-  .home-card {
-    width: 50vw;
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
 </style>
--- ptcg.client/src/services/CardsService.js
+++ ptcg.client/src/services/CardsService.js
@@ -1,10 +1,14 @@
+import { AppState } from "../AppState.js";
 import { logger } from "../utils/Logger.js"
 import { pokemonapi } from "./AxiosService.js"
 
 class CardsService {
   async getCards() {
     const res = await pokemonapi.get('cards')
-   console.log(res.data);
+    console.log(res.data);
+    
+    AppState.cards = res.data
+    // console.log(AppState.cards);
   }
 
 }
